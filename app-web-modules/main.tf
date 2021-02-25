5569247171a98880d4b07fb43d78fd8cb69ba931
psambath <sambath@gmail.com>
2021-02-25 14:59:18 -0800

    Update main.tf

--- app-web-modules/main.tf
+++ app-web-modules/main.tf
@@ -2,7 +2,9 @@ provider "aws" {
 }
 
 module "server" {
-  source = "app.terraform.io/app/frb-bird/workspaces/app-web/server/aws"
+ # source = "app.terraform.io/app/frb-bird/workspaces/app-web/server/aws"
+    source  = "app.terraform.io/frb-bird/server/aws"
+  version = "1.0.0"
   ami                    = var.ami
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
